493895ca72e0bf7bdb0e8843621aa89c03b81116
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-01-14 04:40:55 +0000

    #765 - Reverted correct symfony path

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '/home/lukas/devtoolsphp/symfony/1.4/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
