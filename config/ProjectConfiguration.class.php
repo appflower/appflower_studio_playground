9f65e8801d48d8956c8b9ac8bbed777ec37b2cb8
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-01-14 04:25:40 +0000

    #765 - Soon we'll try to init current project based on some session or url parameter

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,13 +1,50 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once '/home/lukas/devtoolsphp/symfony/1.4/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+    /**
+     * stores current project if current request contains project_slug
+     * @var Project
+     */
+    private $currentProject;
+
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
     $this->enableAllPluginsExcept(array('sfDoctrinePlugin','sfPropelPlugin'));
+    $this->dispatcher->connect('context.load_factories', array($this, 'configureProject'));
+  }
+
+  public function configureProject()
+  {
+      $context = sfContext::getInstance();
+      if (!$context) {
+          return;
+      }
+
+      $request = $context->getRequest();
+
+      if (!$request) {
+          return;
+      }
+
+//      $this->currentProject = ProjectQuery::create()->findOneBySlug('seedcontrol');
+//      return;
+
+//      if ($request->hasParameter('project_slug')) {
+//          $projectSlug = $request->getParameter('project_slug');
+//          $this->currentProject = ProjectQuery::create()->findOneBySlug($projectSlug);
+//          if (!$this->currentProject) {
+//              throw new Exception("Could not find project with slug: $projectSlug");
+//          }
+//      }
+  }
+
+  public function getCurrentProject()
+  {
+      return $this->currentProject;
   }
 }
