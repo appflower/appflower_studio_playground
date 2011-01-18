07cf1a4aa3e35f644b2392ab613fb87d7f8ad027
Radu Topala <radu.topala@trisoft.ro>
2011-01-18 12:51:07 +0000

    changed tools

--- batch/svn_up.php
+++ batch/svn_up.php
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+CLEANUP=( "./plugins/afGuardPlugin" "./plugins/appFlowerPlugin" "./plugins/appFlowerStudioPlugin" "./plugins/sfPropelSqlDiffPlugin" "./plugins" "./lib/vendor/symfony" "./lib/vendor" "." )
+
+
 DIR=$(dirname $0)
 cd $DIR/../
 date
 rm -f ./config/schema.yml
+echo SVN cleanup
+for (( i = 0 ; i < ${#CLEANUP[@]} ; i++ ))
+do
+	svn cleanup ${CLEANUP[$i]}
+	echo ${CLEANUP[$i]} cleaned up
+done
 svn up
 ./symfony propel:build-model
 ./symfony cc
