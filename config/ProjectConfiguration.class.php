bc5125c0b3764055ef3c32b603046ab813b40562
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-01-27 01:38:26 +0000

    #765 - One more commit reverted - r286

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -5,46 +5,9 @@ sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
-    /**
-     * stores current project if current request contains project_slug
-     * @var afsProject
-     */
-    private $currentProject;
-
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
     $this->enableAllPluginsExcept(array('sfDoctrinePlugin','sfPropelPlugin'));
-    $this->dispatcher->connect('context.load_factories', array($this, 'configureProject'));
-  }
-
-  public function configureProject()
-  {
-      $context = sfContext::getInstance();
-      if (!$context) {
-          return;
-      }
-
-      $request = $context->getRequest();
-
-      if (!$request) {
-          return;
-      }
-
-//      $this->currentProject = afsProjectQuery::create()->findOneBySlug('seedcontrol');
-//      return;
-
-//      if ($request->hasParameter('project_slug')) {
-//          $projectSlug = $request->getParameter('project_slug');
-//          $this->currentProject = afsProjectQuery::create()->findOneBySlug($projectSlug);
-//          if (!$this->currentProject) {
-//              throw new Exception("Could not find project with slug: $projectSlug");
-//          }
-//      }
-  }
-
-  public function getCurrentProject()
-  {
-      return $this->currentProject;
   }
 }
