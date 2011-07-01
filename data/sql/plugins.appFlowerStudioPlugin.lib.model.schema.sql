71df7aff2328525b9458c92181a247c60b9d5be7
root <root@www.appflower.com>
2011-07-01 13:24:23 +0400

    regenerated sql chema files

--- data/sql/plugins.appFlowerStudioPlugin.lib.model.schema.sql
+++ data/sql/plugins.appFlowerStudioPlugin.lib.model.schema.sql
@@ -14,28 +14,11 @@ CREATE TABLE `afs_notification`
 (
 	`message` VARCHAR(255)  NOT NULL,
 	`message_type` VARCHAR(255)  NOT NULL,
-	`user` INTEGER(11) default 0 NOT NULL,
+	`user` VARCHAR(128)  NOT NULL,
 	`ip` VARCHAR(255)  NOT NULL,
 	`created_at` DATETIME,
 	`id` INTEGER  NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-#-----------------------------------------------------------------------------
-#-- afs_project
-#-----------------------------------------------------------------------------
-
-DROP TABLE IF EXISTS `afs_project`;
-
-
-CREATE TABLE `afs_project`
-(
-	`name` VARCHAR(255)  NOT NULL,
-	`path` VARCHAR(255)  NOT NULL,
-	`slug` VARCHAR(50)  NOT NULL,
-	`id` INTEGER  NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `afs_project_U_1` (`slug`)
 ) ENGINE=InnoDB;
 
 # This restores the fkey checks, after having unset them earlier
