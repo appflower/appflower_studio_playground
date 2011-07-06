86d974be1e6f0e63e8455f930d37b819038e2798
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-07-06 10:36:43 +0200

    Fixed sql file syntax to work with sqlite database

--- data/sql/plugins.appFlowerStudioPlugin.lib.model.schema.sql
+++ data/sql/plugins.appFlowerStudioPlugin.lib.model.schema.sql
@@ -1,7 +1,6 @@
 
 # This is a fix for InnoDB in MySQL >= 4.1.x
 # It "suspends judgement" for fkey relationships until are tables are set.
-SET FOREIGN_KEY_CHECKS = 0;
 
 #-----------------------------------------------------------------------------
 #-- afs_notification
@@ -17,9 +16,7 @@ CREATE TABLE `afs_notification`
 	`user` VARCHAR(128)  NOT NULL,
 	`ip` VARCHAR(255)  NOT NULL,
 	`created_at` DATETIME,
-	`id` INTEGER  NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT
+);
 
 # This restores the fkey checks, after having unset them earlier
-SET FOREIGN_KEY_CHECKS = 1;
