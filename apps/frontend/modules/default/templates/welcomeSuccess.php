5e1c5fbbf514e69c640ac7c429d2be9af1e5657f
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-07-25 12:16:03 +0200

    Modified welcome screen a little

--- apps/frontend/modules/default/templates/welcomeSuccess.php
+++ apps/frontend/modules/default/templates/welcomeSuccess.php
@@ -7,21 +7,8 @@
     </div>
     <div id="primary_content">
       <h2>The AppFlower Studio</h2>
-      <p>Need to make an enterprise application? Let AppFlower do some magic for you! AppFlower does all the dirty work automatically, allowing you to concentrate on the real business needs.</p>
-      <div id="options">
-        <div class="white_box" id="playground">
-          <h3>Open <span>Appflower</span> Studio</h3>
-          <p>This gets you quickly started on building applications with AppFlower. You can use AppFlower Studio to design models, views, layouts, do code editing and debugging, and much more.</p>
-          <a class="button" target="_blank" href="/studio">Start now</a>
-        </div>
-        <div class="white_box" id="example">
-<!--          <h3>Open <span>seedControl</span> Project</h3>
-          <p>We've included the sample project, SeedControl, to show you how an application can be made. SeedControl is a bug tracking application system that includes role-based access control, among other features. You can read more about this application at <a class="normal" href="http://www.seedcontrol.com">seedcontrol.com</a>.</p>
-          <a class="button" target="_blank" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8081/studio">Start now</a>
--->
-        </div>
-        <div class="clear"></div>
-      </div>      
-      </div>      
+      <p>Welcome to your new AppFlower project. AppFlower enables you to easily make data-oriented applications. To get started making your first application just fire up the visual designer <a target="_blank" href="http://studio.appflower.com/studio">Studio</a>. You can use AppFlower Studio to create an entire application, without the need to edit the project with a traditional IDE.</p>
+
+      <p class="link" style="font-size: 26px"><a class="button" target="_blank" href="http://studio.appflower.com/studio">Start Studio</a></p>
     </div>  
   </div>
