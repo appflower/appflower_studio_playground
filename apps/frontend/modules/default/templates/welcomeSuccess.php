a063e6779b86b92e6986041168514c777a7ca0ce
Radu Topala <radu@appflower.com>
2011-08-02 19:00:41 +0300

    changed welcome screen links to local Studio

--- apps/frontend/modules/default/templates/welcomeSuccess.php
+++ apps/frontend/modules/default/templates/welcomeSuccess.php
@@ -7,8 +7,8 @@
     </div>
     <div id="primary_content">
       <h2>The AppFlower Studio</h2>
-      <p>Welcome to your new AppFlower project. AppFlower enables you to easily make data-oriented applications. To get started making your first application just fire up the visual designer <a target="_blank" href="http://studio.appflower.com/studio">Studio</a>. You can use AppFlower Studio to create an entire application, without the need to edit the project with a traditional IDE.</p>
+      <p>Welcome to your new AppFlower project. AppFlower enables you to easily make data-oriented applications. To get started making your first application just fire up the visual designer <a target="_blank" href="/studio">Studio</a>. You can use AppFlower Studio to create an entire application, without the need to edit the project with a traditional IDE.</p>
 
-      <p class="link" style="font-size: 26px"><a class="button" target="_blank" href="http://studio.appflower.com/studio">Start Studio</a></p>
+      <p class="link" style="font-size: 26px"><a class="button" target="_blank" href="/studio">Start Studio</a></p>
     </div>  
   </div>
