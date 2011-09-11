6cb5ef0c9796ec00ec8c7e162ca24f9699901d6b
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-09-11 04:30:45 +0200

    Added script used to render /etc/issue file after VM boot process

---
+++ batch/render_issue_file.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+$ipAddr = `ifconfig eth0 | grep inet\ addr | cut -f2 -d: | cut -f1 -d\  2> /dev/null`;
+$ipAddr = trim($ipAddr);
+
+$issueContent=
+'
+To use your AppFlower Virtual Machine - open below URL in your browser:
+
+http://'.$ipAddr.'/
+
+';
+$appflowerAscii = file_get_contents('/root/scripts/appflower.ascii');
+$appflowerAscii = str_replace('\\', '\\\\', $appflowerAscii);
+file_put_contents('/etc/issue', $appflowerAscii.$issueContent);
+?>
