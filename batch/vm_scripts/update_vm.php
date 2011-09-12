b35feb300746f015cf309d7df47f67bdf4fce2d0
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-09-12 02:19:53 +0200

    After playing with git hooks I've updated "docs" of VM update script

--- batch/vm_scripts/update_vm.php
+++ batch/vm_scripts/update_vm.php
@@ -11,4 +11,15 @@
  * If VM is used like we intended - this problem should not occure because new projects are clone's of main repository
  *   and while cloning repo - git hooks are not cloned so this script should be hooked only to main studio project
  * However there is never too much of caution :)
+ * 
+ * Also one more important warning.
+ * If there will be a use that is pulling many changes with one 'git pull'
+ *   (someone that has old vmware image and he did not used it for a year)
+ *   then this script will be launched only once and possibly there will be
+ *   many "changes" to apply and they should be applied only once and in correct
+ *   order.
+ *   This means we should implement here in this script something like propel
+ *   migrations mechanism. So each change should be timestamped so we know
+ *   the correct order and also we need to keep track of wchich changes was
+ *   already applied to know where we should start from
  */
