c0c6600e9c9b1894a2fced50320bce937c8ba9be
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-09-12 05:22:49 +0200

    Welcome screen - Added list of projects found in current environment

--- apps/frontend/modules/default/actions/actions.class.php
+++ apps/frontend/modules/default/actions/actions.class.php
@@ -2,6 +2,15 @@
 class defaultActions extends CustomActions
 {
   public function executeWelcome()
-  {  	
+  {
+      try {
+          $serverEnv = afStudioUtil::getServerEnvironment();
+          $vhosts = $serverEnv->getExistingVhosts();
+          $this->vhosts = $vhosts->getAll();
+      } catch(ServerException $e) {
+          if (sfConfig::get('sf_environment') == 'dev') {
+              throw $e;
+          }
+      }
   }	
 }
