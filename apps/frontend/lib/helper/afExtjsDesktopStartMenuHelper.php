53c0eca578fe5ee1df02489eb2e3df91153564cc
Sergey Startsev <startsev.sergey@gmail.com>
2011-12-05 12:33:28 +0200

    #1294 - use afExtjsDesktopStartMenu Builder

--- apps/frontend/lib/helper/afExtjsDesktopStartMenuHelper.php
+++ apps/frontend/lib/helper/afExtjsDesktopStartMenuHelper.php
@@ -1,7 +1,3 @@
 <?php
-$startmenu = new afExtjsStartMenu(array('title'=>'AppFlower'));
 
-$dashboard = new afExtjsStartMenuButton($startmenu,array('label'=>'<img src="/images/famfamfam/house_go.png" border="0">','handler'=>'afApp.widgetPopup(\'/pages/dashboard\');','tooltip'=>array('text'=>'Your overview', 'title'=>'Project Dashboard')));$dashboard->end();
-
-$startmenu->end();
-?>
+afExtjsDesktopStartMenuBuilder::create('frontend', 'app')->process()->getMenuInstance()->end();
