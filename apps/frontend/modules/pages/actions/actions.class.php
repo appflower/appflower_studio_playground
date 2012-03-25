f99f9452f3ce85299c9f017aae90e6fceec97a2c
Sergey Startsev <startsev.sergey@gmail.com>
2012-03-25 20:29:31 +0300

    #1375 - fixed first point from description

--- apps/frontend/modules/pages/actions/actions.class.php
+++ apps/frontend/modules/pages/actions/actions.class.php
@@ -12,39 +12,13 @@ class pagesActions extends CustomActions
      *
      * @param sfWebRequest $request 
      */
-    public function executeError404Page(sfWebRequest $request)
-    {
-        $this->init = true;
-        $this->title = "Page Not Found";
-        $this->html = "<div style = 'padding:10px;text-align:center; font-weight:bold;height:100px; padding-top:30px;color:red'>The requested page not found.</div>";
-        $this->current = 1;
-        $this->id = $request->getParameter('id', 'new');
-        $parser = new XmlParser(true);
-        $this->layout = $parser->getLayout();
-        $this->setLayout("layoutExtjs");
-        $this->setTemplate("ext");
-        
-        return sfView::SUCCESS;
-    }
+    public function executeError404Page() {}
     
     /**
      * Error 500 page
      *
      * @param sfWebRequest $request 
      */
-    public function executeError500Page(sfWebRequest $request)
-    {
-        $this->init = true;
-        $this->title = "Internal Server Error";
-        $this->html = "<div style = 'padding:10px;text-align:center; height:100px; padding-top:30px;color:red'><b>The page can not be displayed.</b><br>Some internal server error has occured.</div>";
-        $this->current = 1;
-        $this->id = $request->getParameter('id', 'new');
-        $parser = new XmlParser(true);
-        $this->layout = $parser->getLayout();
-        $this->setLayout("layoutExtjs");
-        $this->setTemplate("ext");
-        
-        return sfView::SUCCESS;
-    }
+    public function executeError500Page() {}
     
 }
