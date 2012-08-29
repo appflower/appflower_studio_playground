67bf84914a7f4adb56b0f960794e64de59988e82
Michal Piotrowski <michal@eventhorizon.pl>
2012-08-29 14:48:24 +0200

    enable debugging for production environment

--- web/index.php
+++ web/index.php
@@ -4,5 +4,6 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+//$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
